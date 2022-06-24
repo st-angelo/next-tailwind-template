61b42c732f7874df5819682aa133771b9b4ad191
AngeloStatescu <35111313+AngeloStatescu@users.noreply.github.com>
2022-06-24 17:03:17 +0300

    template/1.0

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen font-semibold text-4xl">
+      #TODO
+    </div>
+  );
+}
